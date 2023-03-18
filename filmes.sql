2ad9fd01c0ca407e755e1fc2244ac8e7f5b0d910
PEDROHCA18 <114013463+PEDROHCA18@users.noreply.github.com>
2023-03-18 23:25:30 +0000

    Initial commit

---
+++ filmes.sql
@@ -0,0 +1,30 @@
+create database filmes;
+use filmes;
+
+create table filme(
+codigo int,
+titulo varchar(150),
+codigo_diretor int unique,
+foreign key diretor (codigo_diretor) references filmes (codigo),
+primary key (codigo));
+
+
+create table diretor (
+codigo int,
+nome varchar(100),
+codigo_filme int ,
+foreign key filme (codigo_filme) references diretor (codigo),
+primary key (codigo));
+
+insert into filme values 
+('1','Rambo','1');
+
+insert into diretor values 
+('1','Pedro','1');
+
+select * from filme;
+
+
+
+
+
